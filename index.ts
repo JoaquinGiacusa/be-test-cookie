53f2d57e0ab02b5d65fd889edf150efd1388c8f4
JoaquinGiacusa <joaquingiacusa.dev@gmail.com>
2023-02-12 16:12:41 -0300

    add cors config

--- index.ts
+++ index.ts
@@ -4,6 +4,16 @@ import cookieParser from "cookie-parser";
 import cookie, { serialize } from "cookie";
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 
@@ -13,8 +23,10 @@ app.get("/test", async (req, res) => {
     // httpOnly: true,
     path: "/",
     maxAge: 1000 * 60 * 60 * 24 * 7,
+    // domain: "ec2-100-26-232-24.compute-1.amazonaws.com",
   });
   res.setHeader("Set-Cookie", serialized);
+
   return res.json({ message: "test" });
 });
 
